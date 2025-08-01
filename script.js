399139942ffe568cb678212f51541c9e5e705687
Kalitha Giovanna de Queiroz Rosa <kalitha.rosa@mds.gov.br>
2025-08-01 16:27:46 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,186 @@
+// let maior = Math.max(6,12,18,67,70,4, 32)
+// console.log(maior);
+
+// let menor = Math.min(8,7,6,80,23,1,2)
+// console.log(menor);
+
+
+//==========================================================
+
+// let idade = 20
+// if(idade >18){
+//     console.log('pode entrar')
+// }
+// else{
+//     console.log('nao pode entrar')
+// }
+
+//==========================================================
+
+// let x = 10
+// while(x > 0){
+//     console.log("O x é: " + x)
+//     x = x - 1;
+// }
+
+// let y = 0
+// while(y<=10){
+//     console.log("O y é: " + y)
+//     y = y + 1
+// }
+
+//==========================================================
+
+// for(let i = 0; i < 100; i = i + 3){
+
+//     console.log(`A soma de i com 2 é: ${i+2}`)
+// }
+ 
+//==========================================================
+
+// let nome = "Vitor";
+// for(let i = 0; i < 10; i = i + 1){
+
+//     if(i==3){
+//         nome = "maria";
+
+//     }
+
+//     if(i==5 && nome == "maria"){
+
+//         console.log("O nome é maria, pode parar")
+//         break;
+//     }
+//         console.log(i);
+// }
+
+
+//==========================================================
+// let nome = "vitor"
+// switch (nome){
+//     case "joao":
+//         console.log("o seu nome é joao");
+//         break;
+//     case "marcos":
+//         console.log("o seu nome é marcos");
+//         break;
+//     default:
+//         console.log("o seu nome nao é joao nem marcos!")
+//         break;
+// }
+
+//==========================================================
+//exercicio1
+
+// let nome = "Vitor";
+// let idade = 20;
+// let cnh = true;
+
+// console.log(typeof nome);
+// console.log(typeof idade)
+// console.log(typeof cnh)
+
+
+//==========================================================
+//exercicio2
+// let idade = 18;
+
+// if (idade >= 18){
+//     console.log("pode entrar")
+// }
+// else{
+//     console.log("Nao pode entrar")
+// }
+
+
+
+//==========================================================
+//exercicio3
+// let nome = "vitor";
+// switch(nome){
+//     case "joao":
+//         console.log("acesso negado");
+//         break
+//     case "vitor":
+//         console.log("acesso liberado")
+//         break
+// }
+
+//==========================================================
+//exercicio4
+
+// const num = 18;
+// console.log(Math.pow(2,2));
+// console.log(Math.pow(3,2));
+// console.log(Math.pow(num,2));
+
+//==========================================================
+//exercicio5
+// let VelocidadeCarro = 70;
+//     if (VelocidadeCarro > 80){
+//         console.log("Levou multa")
+//     }
+//     else{
+//         console.log("dentro do limite")
+//     }
+
+//==========================================================
+// exercicio6
+
+// let i = 0
+// while(i <=10){
+//     console.log(i);
+//     i++;
+// }
+
+//==========================================================
+// exercicio7
+
+// for (let i = 100; i >= 50; i--){
+//     console.log(i);
+// }
+
+//==========================================================
+//exercicio8
+
+// for(let i = 0; i <= 50; i++){
+
+//     if(i % 2 == 0){
+//         console.log(`o numero ${i} é par`)
+//     }
+//     else{
+//         console.log(`o numero ${i} é impar`)
+//     }
+// }
+//==========================================================
+
+// function imprimirNoConsole(){
+//     console.log("hello world")
+// }
+
+// imprimirNoConsole();
+
+// function imprimirUmNumero(num){
+//     console.log("O numero é: "+ num)
+// }
+
+// imprimirUmNumero(2);
+
+//==========================================================
+
+// const soma = function(a,b){
+//     return a + b;
+// }
+
+// console.log(soma(3,5));
+
+// const saudacao = function(nome){
+//     if(nome == "vitor"){
+//         return "ola vitor";
+//     }
+// }
+
+// console.log(saudacao("vitor"));
+
+//==========================================================
+
